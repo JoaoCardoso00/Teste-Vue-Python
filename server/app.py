e718fc46a6c7b64cd1a890d34d718d028b2d2de4
JoaoCardoso00 <jvcq13@gmail.com>
2022-11-02 01:59:28 -0300

    now reading data csv

--- server/app.py
+++ server/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import pandas as pd
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
-    return 'This is my first API call!'
 
+    df = pd.read_csv('data/Relatorio_operadoras.csv',encoding='latin-1', on_bad_lines='skip')
+
+    return df.to_html()
+
+
+
+if(__name__ == '__main__'):
+    app.run(debug=True)
